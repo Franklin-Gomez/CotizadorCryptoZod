6e1faa97b354ac2cc14e8a8b19068c6b2934c4bf
Franklin-Gomez <franklin_043097@hotmail.com>
2024-07-02 21:35:17 -0500

    add form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Formulario from "./Components/Formulario"
 
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
         </h1>
 
         <div className="content">
-
+          <Formulario/>
         </div>
       </div>
     </>
---
+++ src/Components/Formulario.tsx
@@ -0,0 +1,35 @@
+export default function Formulario() {
+    return (
+        <form
+            className="form"
+        >
+            <div className="field">
+                <label htmlFor="currency">Moneda : </label>
+                
+                <select 
+                    name="currency" 
+                    id="currency"
+                >
+
+                    <option value=""> --Seleccione La Moneda -- </option>
+                </select>
+
+            </div>
+
+            <div className="field">
+                <label htmlFor="criptocurrency">Criptomoneda : </label>
+                
+                <select 
+                    name="criptocurrency" 
+                    id="criptocurrency"
+                >
+
+                    <option value=""> --Seleccione La Crypto -- </option>
+                </select>
+
+            </div>
+
+            <input type="submit" value="Cotizar" />
+        </form>
+    )
+}
