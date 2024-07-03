0e3d015a8cfb9261cec2578476913bd862765c40
Franklin-Gomez <franklin_043097@hotmail.com>
2024-07-03 11:33:15 -0500

    add global state with zustand and get petitions to api

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,21 @@
 import { create } from 'zustand'
+import axios from 'axios';
+
+async function getCryptos() {
+    const url = 'https://min-api.cryptocompare.com/data/top/totaltoptiervolfull?limit=20&tsym=USD';
+
+    try {
+        
+        const {data : { Data }} = await axios( url )
+        console.log( Data )
+
+    } catch (error) {
+        console.log( error )
+    }
+}
 
 export const useCryptoStore = create(() => ({ 
     fetchCryptos : () => { 
-        console.log('desde fetch Cryptos')
+        getCryptos()
     }
 }))
